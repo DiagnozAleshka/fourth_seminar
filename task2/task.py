e5a300fc4dde7fd3655c12c3e106737c4a0fe94d
Diagnoz_Aleshka <alekseypsih18@gmail.com>
2022-09-17 20:03:34 +0500

    add task 3

--- task2/task.py
+++ task2/task.py
@@ -1,16 +1,5 @@
-""" Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N. """
-
-x = int(input("введите число: "))
-if x == 1 or x == 2 or x == 3 or x == 5 or x == 7:
-    print("вы ввели простое число")
-
-list1=[]
-i = 2
-while i <= x:
-    if x %i==0:
-        list1.append(i)
-        x //= i
-        i = 2
-    else:
-      i +=1
-print (list1)
+lst = list(map(int, input("Введите числа через пробел:\n").split()))
+print(f"Исходный список: {lst}")
+new_lst = []
+[new_lst.append(i) for i in lst if i not in new_lst]
+print(f"Список из неповторяющихся элементов: {new_lst}")
